ec840e1bfc943cc811c5e7c54957754f7f09f72b
Leonardo Val <leonardo.val@creatartis.com>
2017-11-18 16:13:01 -0300

    Updated `creatartis-grunt`

--- tests/require-config.js
+++ tests/require-config.js
@@ -1,22 +1,11 @@
 // Generated code, please do NOT modify.
-(function () { "use strict";
+(function (config) { "use strict";
 	define([], function () {
-		var config = {
-			"paths": {
-				"capataz-inveniemus": "../build/capataz-inveniemus",
-				"creatartis-base": "../node_modules/creatartis-base/build/creatartis-base.min",
-				"sermat": "../node_modules/sermat/build/sermat-umd",
-				"capataz": "../../capataz.js/build/capataz_node.min",
-				"inveniemus": "../node_modules/inveniemus/build/inveniemus.min",
-				"express": "../../capataz.js/node_modules/express/index",
-				"body-parser": "../../capataz.js/node_modules/body-parser/index",
-				"depd": "../../capataz.js/node_modules/depd/index"
-			}
-		};
 		if (window.__karma__) {
 			config.baseUrl = '/base';
 			for (var p in config.paths) {
-				config.paths[p] = config.paths[p].replace(/^\.\.\//, '/base/');
+				config.paths[p] = config.paths[p]
+					.replace(/^\.\.\//, '/base/');
 			}
 			config.deps = Object.keys(window.__karma__.files) // Dynamically load all test files
 				.filter(function (file) { // Filter test modules.
@@ -27,5 +16,40 @@
 		}
 		require.config(config);
 		console.log("RequireJS configuration: "+ JSON.stringify(config, null, '  '));
+
+		return function (deps, main) {
+			require(deps, function () {
+				var args = Array.prototype.slice.call(arguments);
+				args.forEach(function (module, i) {
+					name = module.__name__ || deps[i];
+					if (window.hasOwnProperty(name)) {
+						console.error("Global `"+ name +"` already defined!");
+					} else {
+						window[name] = module;
+						console.log("Loaded library `"+ deps[i] +"` is available as `window."+
+							name +"`.");
+					}
+				});
+				switch (typeof main) {
+					case 'undefined': break;
+					case 'function': main.apply(window, args); break;
+					default: throw new Error('Invalid main function '+ main +'!');
+				}
+				console.log("Ready.");
+			}, function (err) {
+				console.error(err);
+			});
+		};
 	});
-})();
+})({
+	"paths": {
+		"capataz-inveniemus": "../build/capataz-inveniemus",
+		"creatartis-base": "../node_modules/creatartis-base/build/creatartis-base.min",
+		"sermat": "../node_modules/sermat/build/sermat-umd",
+		"capataz": "../../capataz.js/build/capataz_node.min",
+		"inveniemus": "../node_modules/inveniemus/build/inveniemus.min",
+		"express": "../../capataz.js/node_modules/express/index",
+		"body-parser": "../../capataz.js/node_modules/body-parser/index",
+		"depd": "../../capataz.js/node_modules/depd/index"
+	}
+});
